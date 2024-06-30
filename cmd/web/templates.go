e8a37e086250f884ef1c7135bbc7d6ddf9e8c5a8
Federico Elizondo <federicoelizondom@gmail.com>
2024-06-29 23:19:05 -0600

    Automatically parsing partials

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -7,53 +7,43 @@ import (
 	"github.com/fedeelizondo23/snippetbox/internal/models"
 )
 
-// Define a templateData type to act as the holding structure for
-// any dynamic data that we want to pass to our HTML templates.
-// At the moment it only contains one field, but we'll add more
-// to it as the build progresses.
 type templateData struct {
-	Snippet models.Snippet
+	Snippet  models.Snippet
 	Snippets []models.Snippet
 }
 
-func newTemplateCache() (map [string]*template.Template, error) {
-	// Initialize a new map to act as the cache.
+func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
 
-	// Use the filepath.Glob() function to get a slice of all filepaths that
-	// match the pattern "./ui/html/pages/*.html". This will essentially give
-	// us a slice of all the filepaths for our application 'page' templates
-	// like: [ui/html/pages/home.html ui/html/pages/view.html]
 	pages, err := filepath.Glob("./ui/html/pages/*.html")
 	if err != nil {
 		return nil, err
 	}
 
-	// Loop through the page filepaths one-by-one.
 	for _, page := range pages {
-		// Extract the file name (like 'home.html') from the full filepath
-		// and assign it to the name variable.
 		name := filepath.Base(page)
 
-		// Create a slice containing the filepaths for our base template, any
-		// partials and the page.
-		files := []string{
-			"./ui/html/base.html",
-			"./ui/html/partials/nav.html",
-			page,
-		}
-
-		// Parse the files into a template set.
-		ts, err := template.ParseFiles(files...)
+		// Parse the base template file into a template set.
+		ts, err := template.ParseFiles("./ui/html/base.html")
 		if err != nil {
 			return nil, err
 		}
 
-		// Add the template set to the map, using the name of the page
-		// (like 'home.html') as the key.
+		// Call ParseGlob() *on this template set* to add any partials.
+		ts, err = ts.ParseGlob("./ui/html/partials/*.html")
+		if err != nil {
+			return nil, err
+		}
+
+		// Call ParseFiles() *on this template set* to add any partials.
+		ts, err = ts.ParseFiles(page)
+		if err != nil {
+			return nil, err
+		}
+
+		// Add the template set to the map as normal...
 		cache[name] = ts
 	}
 
-	// Return the map.
 	return cache, nil
 }
