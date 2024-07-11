7b22ef059f106fb7d732719b36480dd344030b1f
Federico Elizondo <federicoelizondom@gmail.com>
2024-07-10 23:22:06 -0600

    Add request logging

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,8 +2,6 @@ package main
 
 import "net/http"
 
-// Update the signature for the routes() method so that it returns a
-// http.Handler instead of *http.ServeMux.
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
@@ -15,8 +13,6 @@ func (app *application) routes() http.Handler {
 	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
 	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
 
-	// Pass the servemux as the 'next' parameter to the commonHeaders middleware.
-	// Because commonHeaders is just a function, and the function returns a
-	// http.Handler we don't need to do anything else.
-	return commonHeaders(mux)
+	// Wrap the existing chain with the logRequest middleware.
+	return app.logRequest(commonHeaders(mux))
 }
