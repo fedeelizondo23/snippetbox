308c137bcca714d5bd31ba68fdbf40a85a6a437a
Federico Elizondo <federicoelizondom@gmail.com>
2024-07-21 11:56:06 -0600

    Displaying errors and repopulating fields

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -8,21 +8,18 @@ import (
 	"github.com/fedeelizondo23/snippetbox/internal/models"
 )
 
+// Add a Form field with the type "any".
 type templateData struct {
 	CurrentYear int
 	Snippet     models.Snippet
 	Snippets    []models.Snippet
+	Form        any
 }
 
-// Create a humanDate function which returns a nicely formatted string
-// representation of a time.Time object.
 func humanDate(t time.Time) string {
 	return t.Format("02 Jan 2006 at 15:04")
 }
 
-// Initialize a template.FuncMap object and store it in a global variable. This is
-// essentially a string-keyed map which acts as a lookup between the names of our
-// custom template functions and the functions themselves.
 var functions = template.FuncMap{
 	"humanDate": humanDate,
 }
